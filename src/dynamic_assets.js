5f5c4732b6b16b33d043dc6ae3962c6a14bad7f0
isaiah-gordon <isaiah.gordon.developer@gmail.com>
2021-06-07 00:47:15 -0300

    Create module "dynamic_assets.js" Create file "dynamic_assets_data.json" for new module to work with. Clean up "script.js" a little.

---
+++ src/dynamic_assets.js
@@ -0,0 +1,96 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as dat from 'dat.gui'
+
+import gsap from 'gsap'
+
+// Import my modules.
+import * as Builder from './my_three_module.js'
+import * as Assets from './dynamic_assets.js'
+
+const ASSET_DATA = require("./dynamic_assets_data.json");
+
+function getJSONPath(path) {
+    var res = path.split('.').reduce(function (x, y) {
+        return x && x[y];
+    }, ASSET_DATA);
+    return res
+}
+
+class Create {
+    constructor(scene_arg) {
+        this.scene_arg = scene_arg
+
+        //this.load = new Builder.Load(scene_arg)
+        //this.create = new Builder.Create(scene_arg)
+    }
+
+    button(name, mouse, camera, position) {
+
+        function pathToKey(key) {
+            return getJSONPath('buttons.' + name + '.' + key)
+        }
+
+        const load = new Builder.Load(this.scene_arg)
+        const create = new Builder.Create(this.scene_arg)
+
+        var BUTTON_MODEL = load.model(pathToKey('model_source'), position, [0, 0, 0], [0.7, 0.7, 0.7])
+
+        var TOP_LIGHT_Y = position[1] + 0.01
+        var BOTTOM_LIGHT_Y = position[1] + 0.09
+
+        var BUTTON_TOP_LIGHT = create.pointLight(0, pathToKey('top_light_color'), [position[0], TOP_LIGHT_Y, position[2]])
+        var BUTTON_BOTTOM_LIGHT = create.pointLight(0, pathToKey('bottom_light_color'), [position[0], BOTTOM_LIGHT_Y, position[2]])
+
+        let objs = [BUTTON_MODEL]
+        const BUTTON_ANIMATION_Y = position[1] + 0.03
+
+        function render() {
+
+            const raycaster = new THREE.Raycaster();
+
+            // update the picking ray with the camera and mouse position
+            raycaster.setFromCamera(mouse, camera);
+
+            // calculate objects intersecting the picking ray
+            const intersects = raycaster.intersectObjects(objs, true) //scene.children
+
+            for (let i = 0; i < intersects.length; i++) {
+
+                gsap.to(BUTTON_MODEL.position, { y: BUTTON_ANIMATION_Y, duration: 0.5 })
+                gsap.to(BUTTON_TOP_LIGHT, { intensity: 2, duration: 0.5 })
+                gsap.to(BUTTON_BOTTOM_LIGHT, { intensity: 0.5, duration: 0.5 })
+                document.body.style.cursor = "pointer";
+
+                return name
+
+            }
+
+            for (const object of objs) {
+                if (!intersects.find(intersect => intersect.object != object)) {
+                    gsap.to(BUTTON_MODEL.position, { y: position[1], duration: 0.5 })
+                    gsap.to(BUTTON_TOP_LIGHT, { intensity: 0, duration: 0.5 })
+                    gsap.to(BUTTON_BOTTOM_LIGHT, { intensity: 0, duration: 0.5 })
+                }
+            }
+
+        }
+
+        function openWebsite() {
+            var object = render()
+            if (object == name) {
+                window.open(pathToKey('link_source'))
+            }
+        }
+
+        window.addEventListener('click', openWebsite);
+
+        return render;
+        
+    }
+
+}
+
+export { Create }
--- src/script.js
+++ src/script.js
@@ -6,14 +6,10 @@ import * as dat from 'dat.gui'
 
 import gsap from 'gsap'
 
-// Import my module.
+// Import my modules.
 import * as Builder from './my_three_module.js'
+import * as Assets from './dynamic_assets.js'
 
-// Loader
-const dot_texture = new THREE.TextureLoader().load('textures/pixel_dot_background.png')
-dot_texture.wrapS = THREE.RepeatWrapping;
-dot_texture.wrapT = THREE.RepeatWrapping;
-dot_texture.repeat.set(200, 200);
 
 // Debug
 const gui = new dat.GUI()
@@ -24,97 +20,13 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-
-// = = = = = = MY THREE MODULE = = = = = =
-// Using my module to build some text geometry
-
-const load = new Builder.Load(scene, gui)
-const create = new Builder.Create(scene)
-const debug = new Builder.Debug(gui)
-
-// GitHub Button
-var gitHub = load.model('models/buttons/github_button.gltf', [0.54, 0.02, -0.7], [0, 0, 0], [0.7, 1, 0.7])
-var linkedIn = load.model('models/buttons/linkedin_button.gltf', [0.3, 0.02, -0.7], [0, 0, 0], [0.7, 1, 0.7])
-debug.object(linkedIn, "Linked In")
-
-const gitHubLight1 = create.pointLight(0, 0x6600cd, [0.54, 0.11, -0.7])
-const gitHubLight2 = create.pointLight(0, 0x6600cd, [0.54, 0.03, -0.7])
-
-
-var nameStatue = load.text(undefined, "ISAIAH GORDON", 0.11, 0xf2f2f2, 0.03, [-0.47, 0.11, -1], [-0.26, 0, 0])
-debug.object(nameStatue, "Name Statue")
-
-load.text(undefined, "Developer & IT Pro", 0.085, 0xb50018, 0.02, [-0.33, 0.03, -0.92], [-0.29, 0, 0], "Title Statue")
-
-// SPOTLIGHT 1
-const spotLight1 = new create.spotLight(1.22, 0xd0caf7, [3.54, 8, 7.4], [0.1, 0.01, -0.9])
-scene.add(spotLight1)
-debug.light(spotLight1, 'Spot light 1')
-
-// SPOTLIGHT 2
-const spotLight2 = new create.spotLight(0, 0xff7979, [0.14, 1.66, 2.11], [0.2, 0.01, 1])
-scene.add(spotLight2)
-
-// Spotlight Transitions
-document.addEventListener('scroll', () => {
-    // console.log(window.scrollY)
-    if (window.scrollY < 780) {
-        gsap.to(spotLight1, { intensity: 1.22, duration: 1 })
-        gsap.to(spotLight2, { intensity: 0, duration: 1 })
-    }
-    if (window.scrollY > 780) {
-        gsap.to(spotLight1, { intensity: 0, duration: 1 })
-        gsap.to(spotLight2, { intensity: 1.5, duration: 1 })
-    }
-})
-
-load.text(undefined, "PROJECTS", 0.1, 0xf2f2f2, 0.02, [-0.323, 0.01, 0.669], [0, 0, 0])
-
-var longText = `
-Too much garbage in your face? There is
-plenty of space out in space! BnL Starliners
-leaving each day. We'll clean up the mess
-while you're away.
-`
-
-load.text(undefined, longText, 0.037, 0x545454, 0, [-0.32, 0.001, 0.78], [-1.571, 0, 0])
-
-load.text(undefined, "SKILLS", 0.1, 0xf2f2f2, 0.02, [-0.103, 0.01, 4], [0, 0.4, 0])
-
-// The sun is a deadly lazer!
-const hemLight = new THREE.HemisphereLight(0xbbddff, 0x080820, 0.1);
-scene.add(hemLight);
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2);
-scene.add(directionalLight);
-
-
-// Objects
-const floor_plane = new THREE.PlaneGeometry(30.0, 30.0, 1, 1)
-
-
-// Materials
-const material = new THREE.MeshStandardMaterial({ map: dot_texture, side: THREE.DoubleSide });
-material.roughness = 0.8
-
-
-// Mesh
-// Scene Floor
-const floor = new THREE.Mesh(floor_plane, material)
-scene.add(floor)
-
-floor.rotation.x = 1.5708
-
-/**
- * Sizes
- */
+// Sizes
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -130,14 +42,10 @@ window.addEventListener('resize', () =>
 
 // Camera
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
-
 camera.position.set(0.14, 0.65, 0);
 camera.rotation.set(-0.84, 0, 0)
-
 camera.zoom = 0.7
 camera.fov = 45
-
-console.log(innerWidth)
 
 // Mobile Responsive Camera Settings
 if (innerWidth < 1000) {
@@ -155,25 +63,90 @@ window.addEventListener('resize', function () {
 });
 
 camera.updateProjectionMatrix();
-
 scene.add(camera)
 
-const camera_angle = gui.addFolder('Camera')
-
-camera_angle.add(camera.position, 'x').min(-1).max(2).step(0.01).name("X Pos")
-camera_angle.add(camera.position, 'y').min(-1).max(3).step(0.01).name("Y Pos")
-camera_angle.add(camera.position, 'z').min(-1).max(1).step(0.01).name("Z Pos")
-
-camera_angle.add(camera.rotation, 'x').min(-2).max(2).step(0.01).name("X Rot")
-camera_angle.add(camera.rotation, 'y').min(-2).max(2).step(0.01).name("Y Rot")
-camera_angle.add(camera.rotation, 'z').min(-2).max(2).step(0.01).name("Z Rot")
+const debug = new Builder.Debug(gui)
+debug.object(camera, "Camera")
 
 
-// Controls
+// Sky Light
+// (The sun is a deadly lazer!)
+const hemLight = new THREE.HemisphereLight(0xbbddff, 0x080820, 0.1);
+scene.add(hemLight);
 
-/**
- * Renderer
- */
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2);
+scene.add(directionalLight);
+
+// Scene Floor
+const dot_texture = new THREE.TextureLoader().load('textures/pixel_dot_background.png')
+dot_texture.wrapS = THREE.RepeatWrapping;
+dot_texture.wrapT = THREE.RepeatWrapping;
+dot_texture.repeat.set(200, 200);
+
+const floor_plane = new THREE.PlaneGeometry(30.0, 30.0, 1, 1)
+
+const floor_material = new THREE.MeshStandardMaterial({ map: dot_texture, side: THREE.DoubleSide });
+floor_material.roughness = 0.8
+
+const floor = new THREE.Mesh(floor_plane, floor_material)
+scene.add(floor)
+
+floor.rotation.x = 1.5708
+
+// = = = = = = MY THREE MODULE = = = = = =
+// Using my module to build some stuff!
+
+const load = new Builder.Load(scene, gui)
+const create = new Builder.Create(scene)
+
+const addAsset = new Assets.Create(scene)
+
+
+var nameStatue = load.text(undefined, "ISAIAH GORDON", 0.11, 0xf2f2f2, 0.03, [-0.47, 0.11, -1], [-0.26, 0, 0])
+debug.object(nameStatue, "Name Statue")
+
+load.text(undefined, "Developer & IT Pro", 0.085, 0xb50018, 0.02, [-0.33, 0.03, -0.92], [-0.29, 0, 0], "Title Statue")
+
+// SPOTLIGHT 1
+const spotLight1 = new create.spotLight(0, 0xd0caf7, [3.54, 8, 7.4], [0.1, 0.01, -0.9])
+scene.add(spotLight1)
+
+// SPOTLIGHT 2
+const spotLight2 = new create.spotLight(1.22, 0xd0caf7, [0.14, 1.66, 2.11], [0.2, 0.01, 1])
+scene.add(spotLight2)
+debug.light(spotLight2, 'Spot Light')
+
+//// Spotlight Transitions
+//document.addEventListener('scroll', () => {
+//    // console.log(window.scrollY)
+//    if (window.scrollY < 780) {
+//        gsap.to(spotLight1, { intensity: 1.22, duration: 1 })
+//        gsap.to(spotLight2, { intensity: 0, duration: 1 })
+//    }
+//    if (window.scrollY > 780) {
+//        gsap.to(spotLight1, { intensity: 0, duration: 1 })
+//        gsap.to(spotLight2, { intensity: 1.5, duration: 1 })
+//    }
+//})
+
+load.text(undefined, "PROJECTS", 0.1, 0xf2f2f2, 0.02, [-0.323, 0.01, 0.669], [0, 0, 0])
+
+var longText = `
+Too much garbage in your face? There is
+plenty of space out in space! BnL Starliners
+leaving each day. We'll clean up the mess
+while you're away.
+`
+
+load.text(undefined, longText, 0.037, 0x545454, 0, [-0.32, 0.001, 0.78], [-1.571, 0, 0])
+
+load.text(undefined, "SKILLS", 0.1, 0xf2f2f2, 0.02, [-0.103, 0.01, 4], [0, 0.4, 0])
+
+// Camera here?
+
+
+// Renderer
+ 
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
     alpha: true
@@ -181,9 +154,8 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-// = = = = = = = = = = = ANIMATIONS = = = = = = = = = = = = = = =
 
-// = = = = = Raycasting = = = = =
+// = = = = = = = = = = = ANIMATIONS = = = = = = = = = = = = = = =
 
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
@@ -195,76 +167,11 @@ function onMouseMove(event) {
 
 }
 
-console.log(scene.children)
-
-
-let objs = [scene.children[0]]
-console.log(objs[0].children)
-function render() {
-
-    // update the picking ray with the camera and mouse position
-    raycaster.setFromCamera(mouse, camera);
-
-    // calculate objects intersecting the picking ray
-    const intersects = raycaster.intersectObjects(objs, true) //scene.children
-
-    for (let i = 0; i < intersects.length; i++) {
-
-        gsap.to(gitHub.position, { y: 0.05, duration: 0.5 })
-        gsap.to(gitHubLight1, { intensity: 0.5, duration: 0.5 })
-        gsap.to(gitHubLight2, { intensity: 2, duration: 0.5 })
-        //gsap.to(spotLight1, { intensity: 0.5, duration: 0.5 })
-        document.body.style.cursor = "pointer";
-        //gitHub.position.y = -0.05
-        console.log("intersect!")
-
-        return "interact"
-
-    }
-
-    for (const object of objs) {
-        //console.log(!intersects.find(intersect => intersect.object === object))
-        if (!intersects.find(intersect => intersect.object != object)) {
-            gsap.to(gitHub.position, { y: 0.02, duration: 0.5 })
-            gsap.to(gitHubLight1, { intensity: 0, duration: 0.5 })
-            gsap.to(gitHubLight2, { intensity: 0, duration: 0.5 })
-            //gsap.to(spotLight1, { intensity: 1.3, duration: 0.5 })
-            document.body.style.cursor = "default";
-            // tl1.restart()
-            //gitHub.position.y = -0.065
-            console.log("None")
-        }
-    }
-    
-
-    renderer.render(scene, camera);
-
-}
-
 window.addEventListener('mousemove', onMouseMove, false);
 
-window.requestAnimationFrame(render);
-
-function openWebsite() {
-    var object = render()
-    if (object == "interact") {
-        window.open("https://github.com/isaiah-gordon")
-    }
-}
-
-window.addEventListener('click', openWebsite);
-
-
-// document.addEventListener('mousemove', onDocumentMouseMove)
-
-const windowX = window.innerWidth / 2;
-const windowY = window.innerHeight / 2;
-
-function onDocumentMouseMove(event) {
-    // console.log(event)
-    mouseX = (event.clientX - windowX)
-    mouseY = (event.clientY - windowY)
-}
+// Setup Buttons
+const render_github_button = addAsset.button('github', mouse, camera, [0.54, 0.02, -0.7])
+const render_linkedin_button = addAsset.button('linkedin', mouse, camera, [0.3, 0.03, -0.7])
 
 
 const updateCamera = (event) => {
@@ -278,19 +185,16 @@ const clock = new THREE.Clock()
 
 
 // = = = = = Animation Loop = = = = =
-const tick = () =>
-{
+const tick = () => {
 
-    const elapsedTime = clock.getElapsedTime()
-
-    // spotLightHelper.update();
-
-    render()
+    //render()
+    document.body.style.cursor = "default";
+    render_linkedin_button()
+    render_github_button()
+    
 
     // Render
     renderer.render(scene, camera)
-    //console.log(tickCount)
-    //console.log(elapsedTime)
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
